c357c03ea6138a51f61584d846a3d758882e2911
fundamental <mark.d.mccurry@gmail.com>
2017-08-15 19:20:28 -0400

    Update With Tests

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # LCA - Locally Competitive Algorithm
 
-This project is a bare bones implementation of this sparse approximation
+This project is a bare bones implementation of LCA's sparse approximation
 algorithm.
+
+For more details see:
+Rozell, Christopher and Johnson, Don and Baraniuk, Richard and Olshausen, Bruno
+"Locally competitive algorithms for sparse approximation", IEEE International Conference on Image Processing 2007
+
+and subsequent papers on LCA.
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,39 @@
 using LCA
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# Testing params
+M = 100
+N = 100
+
+k = 10
+
+srand(0)
+
+# Dictionary
+basis = randn(M,N)
+
+# Make k sparse signal
+vals  = rand(M)
+vals[sortperm(rand(M))[k+1:end]] = 0
+
+# Input signal with k sparse structure
+sig = basis*vals
+
+# Learn 'vals' based upon sig and the basis
+out = LCA.lca(sig, basis)
+
+recon = basis*out
+
+sparse_orig = vals .!= 0
+sparse_new  = out  .!= 0
+
+sse_orig  = mean(recon.^2)
+sse_new   = mean(sig.^2)
+sse_diff  = mean((sig.-recon).^2)
+
+# Same sparsity found in output
+@test sparse_orig == sparse_new
+
+# Reconstruction models data
+@test sse_orig*0.01 > sse_diff
+@test sse_new *0.01 > sse_diff
