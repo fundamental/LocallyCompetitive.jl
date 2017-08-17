b458d8ad71ccf265b26922efa13130c62a41fb25
Alex Arslan <ararslan@comcast.net>
2017-08-16 20:10:18 -0700

    Expand testing infrastructure and separate example

--- src/LCA.jl
+++ src/LCA.jl
@@ -64,35 +64,4 @@ function lca(s::Vector{Float64}, D::Matrix{Float64}; T_soft::Float64=0.1,
     return a
 end
 
-if(false)
-    # Random Dictionary based example
-    using PyPlot
-
-    dim     = 64
-    n_elems = 1024;
-    D       = randn(dim,n_elems);
-    s       = randn(dim)
-    sparse  = lca(s , D, T_soft=0.4)
-
-    println("Original  Sparsity = ", sum(s.!=0))
-    println("Original  RMS      = ", sqrt(mean(s.^2)))
-    println("Resulting Sparsity = ", sum(sparse.!=0))
-    println("Recovery  RMS      = ", sqrt(mean((s.-D*sparse).^2)))
-
-
-    figure(1)
-    PyPlot.clf()
-    title("Sparse Realization of input")
-    xlabel("Dictionary Element")
-    ylabel("Magnitude")
-    stem(sparse)
-
-    figure(2)
-    PyPlot.clf()
-    title("Reconstruction of random input")
-    plot(s)
-    plot(D*sparse)
-    legend(["Original", "Recovered"])
-end
-
 end # module
